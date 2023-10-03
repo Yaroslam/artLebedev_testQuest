48ad2febef36fa07b929ddf9155169e8d09f183a
Черкашин Ярослав Ю <iaiucherkashin@f-soft.org>
2023-10-03 21:26:58 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 service mysql start
+sleep 5
 echo "start mysql"
 mysql -u root -e "create database lebedevart CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+sleep 5
 echo "create DB"
 mysql -u root -e "create user 'user' identified by 'password'"
+sleep 5
 echo "create user for db"
 mysql -u root -e "grant all privileges on lebedevart.* to 'user'"
+sleep 5
 echo "gratn privileges to user"
 python3 ./artLebedev_testQuest/djangoProject/manage.py migrate
+sleep 5
 python3 ./artLebedev_testQuest/parser/main.py
 
 
